d50782c2673bcce5d2e86a4a53561fc83ea1c373
DonatasNoreika <donoras@gmail.com>
2026-08-07 09:15:47 +0300

    2: +Pridėtas ir registruotas Author modelis

--- mysite/library/models.py
+++ mysite/library/models.py
@@ -6,3 +6,12 @@ class Genre(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Author(models.Model):
+    first_name = models.CharField()
+    last_name = models.CharField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
