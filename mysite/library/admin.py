8a07a91ca9be3c32362811c5ab703cf564a78c02
DonatasNoreika <donoras@gmail.com>
2026-08-10 09:30:04 +0300

    3: +Prie knygos rodomi jos egzemplioriai

--- mysite/library/admin.py
+++ mysite/library/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from .models import Genre, Author, Book, BookInstance
 
+class BookInstanceInLine(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
 class BookAdmin(admin.ModelAdmin):
     list_display = ['title', 'isbn', 'display_genre', 'author']
+    inlines = [BookInstanceInLine]
 
 class BookInstanceAdmin(admin.ModelAdmin):
     list_display = ['uuid', 'book', 'status', 'due_back']
