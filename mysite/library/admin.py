8fbb36029cb8d7d892ddcef43310da66e6475164
DonatasNoreika <donoras@gmail.com>
2026-08-10 08:59:25 +0300

    3: +Nustatyti knygų sąrašo stulpeliai admin puslapyje

--- mysite/library/admin.py
+++ mysite/library/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from .models import Genre, Author, Book, BookInstance
 
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'isbn', 'author']
+
 # Register your models here.
 admin.site.register(Genre)
 admin.site.register(Author)
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
 admin.site.register(BookInstance)
