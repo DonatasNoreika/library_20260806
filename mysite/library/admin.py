752e27589cacdd396655161cc1a16b081c36c995
DonatasNoreika <donoras@gmail.com>
2026-08-10 09:21:18 +0300

    3: +Pritaikytas BookInstance sąrašas

--- mysite/library/admin.py
+++ mysite/library/admin.py
@@ -4,8 +4,11 @@ from .models import Genre, Author, Book, BookInstance
 class BookAdmin(admin.ModelAdmin):
     list_display = ['title', 'isbn', 'display_genre', 'author']
 
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ['uuid', 'book', 'status', 'due_back']
+
 # Register your models here.
 admin.site.register(Genre)
 admin.site.register(Author)
 admin.site.register(Book, BookAdmin)
-admin.site.register(BookInstance)
+admin.site.register(BookInstance, BookInstanceAdmin)
