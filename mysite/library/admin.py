52877207e8de6a886cb4884b759ed0facc7dc57a
DonatasNoreika <donoras@gmail.com>
2026-08-20 08:57:52 +0300

    10: +Sukūrus arba paredagavus vartotoją, automatiškai sukuriamas/paredaguojamas jo profilis

--- mysite/library/admin.py
+++ mysite/library/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Genre, Author, Book, BookInstance, BookReview
+from .models import (Genre,
+                     Author,
+                     Book,
+                     BookInstance,
+                     BookReview,
+                     Profile)
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'display_books']
@@ -44,3 +49,4 @@ admin.site.register(Author, AuthorAdmin)
 admin.site.register(Book, BookAdmin)
 admin.site.register(BookInstance, BookInstanceAdmin)
 admin.site.register(BookReview, BookReviewAdmin)
+admin.site.register(Profile)
--- mysite/library/models.py
+++ mysite/library/models.py
@@ -5,6 +5,14 @@ from django.utils import timezone
 from tinymce.models import HTMLField
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to="profile_pics", null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} profile"
+
 class Genre(models.Model):
     name = models.CharField()
 
